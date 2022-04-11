23b4a7bdaac47fdd58de05460b6d92234bf3f245
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-04-11 17:10:08 -0300

    Enable Swagger

--- Todos.Api/Program.cs
+++ Todos.Api/Program.cs
@@ -1,13 +1,28 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Todos.Api.Data;
 using Todos.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Todos") ?? "Data Source=Todos.db";
 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSqlite<TodosContext>(connectionString);
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "Todos API",
+        Description = "Store and retrieve all your duties.",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+});
 
 app.MapGet("/todos", async (TodosContext db) => await db.Todos.ToListAsync());
 app.MapPost("/todos", async (TodosContext db, Todo todo) =>
