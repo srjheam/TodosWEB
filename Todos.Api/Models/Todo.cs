9d1bca7105790a2c22f61fc5dcbc8fd721e0513c
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-04-11 16:26:36 -0300

    Add Todo Model

---
+++ Todos.Api/Models/Todo.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Todos.Api.Models {
+    class Todo {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
