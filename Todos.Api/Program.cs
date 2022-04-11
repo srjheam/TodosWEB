ee496f80b80e6e03c09623f7248b09a77349ebee
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-04-11 17:04:27 -0300

    Add todos api endpoints

--- Todos.Api/Program.cs
+++ Todos.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Todos.Api.Data;
+using Todos.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Todos") ?? "Data Source=Todos.db";
@@ -8,6 +9,30 @@ builder.Services.AddSqlite<TodosContext>(connectionString);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/todos", async (TodosContext db) => await db.Todos.ToListAsync());
+app.MapPost("/todos", async (TodosContext db, Todo todo) =>
+{
+    await db.Todos.AddAsync(todo);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todos/{todo.Id}", todo);
+});
+app.MapGet("/todos/{id}", async (TodosContext db, int id) => await db.Todos.FindAsync(id));
+app.MapPut("/todos/{id}", async (TodosContext db, Todo updateTodo, int id) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo is null) return Results.NotFound();
+    todo.Title = updateTodo.Title;
+    todo.IsCompleted = updateTodo.IsCompleted;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/todos/{id}", async (TodosContext db, int id) =>
+{
+  var todo = await db.Todos.FindAsync(id);
+  if (todo is null) return Results.NotFound();
+  db.Todos.Remove(todo);
+  await db.SaveChangesAsync();
+  return Results.Ok();
+});
 
 app.Run();
